8b08b6a1d59ea3aece52744fc4ed9d2f1124da42
flar2 <asegaert@gmail.com>
2016-01-30 00:58:45 -0500

    block IPA_WS wakelock

--- drivers/base/power/wakeup.c
+++ drivers/base/power/wakeup.c
@@ -29,6 +29,9 @@ module_param(enable_wlan_wake_ws, bool, 0644);
 static bool enable_bluedroid_timer_ws = true;
 module_param(enable_bluedroid_timer_ws, bool, 0644);
 
+static bool enable_ipa_ws = true;
+module_param(enable_ipa_ws, bool, 0644);
+
 /*
  * If set, the suspend/hibernate code will abort transitions to a sleep state
  * if wakeup events are registered during or immediately before the transition.
@@ -448,6 +451,9 @@ static void wakeup_source_activate(struct wakeup_source *ws)
                 return;
 
 	if (!enable_bluedroid_timer_ws && !strcmp(ws->name, "bluedroid_timer"))
+		return;
+
+	if (!enable_ipa_ws && !strcmp(ws->name, "IPA_WS"))
 		return;
 
 	if (WARN(wakeup_source_not_registered(ws),
